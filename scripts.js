79401791d2332e7b1204cde0cf89458c1f052268
Ike Addy <ikaddy05@gmail.com>
2023-06-12 19:29:36 -0400

    Added bootstrap styling Created book table Changed page styling

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const deleteBtn = document.querySelector(".delete");
+const readBtn = document.querySelector(".rbtn");
+const input = document.getElementById("filter");
+
+input.addEventListener("click", e => {
+    alert('test')
+})
+readBtn.addEventListener("click", e => {
+    alert('test')
+})
+deleteBtn.addEventListener("click", e => {
+    alert('test')
+})
