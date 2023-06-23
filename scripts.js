211ea4c2a596bb313c7eb05e9f7abad228484ba6
Ike Addy <ikaddy05@gmail.com>
2023-06-23 07:40:58 -0400

    Added modal when addBtn is clicked Added functionality for submit btn to create a new div when clicked Next is to add styling to title and author when a new div is created

--- scripts.js
+++ scripts.js
@@ -7,36 +7,38 @@ const closeModal = document.querySelector(".close");
 const submit = document.getElementById("submit");
 const form = document.getElementById("new-book");
 
+submit.addEventListener('click', newBook);
 
-// Function to create a new book after filling out modal form
+// Function to add new book
+function newBook(e) {
+// Prevent submit button from reloading page 
+    e.preventDefault();
+// Create a div with a class called card and a width of 30rem
+    const newCard = document.createElement('div');
+    newCard.className = 'card';
+    newCard.style.width = '30rem';
+// Store the new card inside the body container
+    const cardsContainer = document.querySelector(".body-container");
+    cardsContainer.appendChild(newCard);
+// Create another div inside the card div called cardBody
+    const cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+// Inside the card body, add the book title with an h5 header
+    const form = document.getElementById('new-book');
+    
+    newCard.textContent = form.elements['title'].value;
+// Inside the card body, add the author with h6 and italics
+   
 
-    // When user clicks submit after filling out book info, generate a new book
-    // on the page with the information entered
-
-        // Create a new card
-
-        // Insert title at top of card with h5
-
-        // Insert author in italics & h6
-
-        // Insert checkbox, editBtn, and removeBtn
+}
 
 
 
 // Function to remove book when remove button is clicked
 
-    // Remove card and all styling
 
 
 // Function to edit existing book when edit button is clicked
-
-    // Open modal
-
-    // Change modal header to "Edit Book"
-
-    // Display current information in input section
-
-    // When user clicks submit, update card with new info
 
 
 // Function to show book typed in search bar
