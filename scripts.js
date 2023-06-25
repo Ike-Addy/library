d80c69b87a45fb33bc331663ab791fb3ea94c2f0
Ike Addy <ia@nacua.org>
2023-06-25 07:43:46 -0400

    Empty commit

--- scripts.js
+++ scripts.js
@@ -5,33 +5,66 @@ const editBtn = document.querySelector(".edit-btn");
 const modal = document.getElementById("myModal");
 const closeModal = document.querySelector(".close");
 const submit = document.getElementById("submit");
-const form = document.getElementById("new-book");
+const bookForm = document.getElementById('new-book');
 
-submit.addEventListener('click', newBook);
+
+bookForm.addEventListener('submit', newBook);
 
 // Function to add new book
 function newBook(e) {
-// Prevent submit button from reloading page 
+    // Prevent submit button from reloading page 
     e.preventDefault();
-// Create a div with a class called card and a width of 30rem
+
+    // Create a div with a class called card and a width of 30rem
     const newCard = document.createElement('div');
     newCard.className = 'card';
-// Store the new card inside the body container
+
+    // Stores the new card inside the body container
     const cardsContainer = document.querySelector(".body-container");
     cardsContainer.appendChild(newCard);
-// Create another div inside the card div called cardBody to store and styling card contents
+
+    // Creates another div inside the card div called cardBody to store and style card contents
     const cardBody = document.createElement('div');
     cardBody.className = 'card-body';
+    newCard.appendChild(cardBody);
+
+    // Get the field values
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const description = document.getElementById('description').value;
+
+    // Create text nodes
+    const titleTextNode = document.createTextNode(title);
+    const authorTextNode = document.createTextNode(author);
+    const descriptionTextNode = document.createTextNode(description);
+
+    // Append text nodes to the cardBody element
+    cardBody.appendChild(titleTextNode);
+    cardBody.appendChild(authorTextNode);
+    cardBody.appendChild(descriptionTextNode);
+
+    // Containers to store text for styling
+    const titleStyles = document.createElement('div');
+    titleStyles.className = 'title'
+    titleStyles.appendChild(title);
+    
+    const authorStyles = document.createElement('div');
+    authorStyles.className = 'author'
+    authorStyles.appendChild(title);
+
+    const descriptionStyles= document.createElement('div')
+    descriptionStyles.className = 'description'
+    descriptionStyles.appendChild(title);
+}
+
+
 // Inside the card body, add the book title with an h5 header
     // Get the form from the DOM
-    const form = document.getElementById('new-book');    
     // Input the input value of the title into the card
+    
     // Set the styling of the title to h5
     
 // Inside the card body, add the author with h6 and italics
-   
-
-}
 
 
 
